a761f846d9d86bd8903193236c6e0a7def974cd2
EmorimaDevTeam <ihoj74759@gmail.com>
2025-10-30 17:32:06 +0700

    Update emorimasetup.sh

--- emorimasetup.sh
+++ emorimasetup.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
-cd
-if [ -e "/data/data/com.termux/files/home/storage" ]; then
-    rm -rf /data/data/com.termux/files/home/storage
-fi
 termux-setup-storage
-yes | pkg update && yes | pkg upgrade -y && echo "y" | termux-setup-storage && yes | pkg install -y python python-pip clang make libffi openssl libjpeg-turbo libpng zlib freetype git cmake tsu libexpat && export PIP_NO_CACHE_DIR=0 && pip uninstall -y psutil && pip install --upgrade pip setuptools wheel && pip install --prefer-binary requests pytz pyjwt pycryptodome rich colorama flask pillow psutil discord.py python-socketio prettytable pystyle aiohttp || CFLAGS="-Wno-error=implicit-function-declaration" pip install psutil && echo "Successfully installed the important packages to run the tool, start the tool, this will take a while, prepare your key tool!" && sleep 2 && su -c "cd /sdcard/Download && /data/data/com.termux/files/usr/bin/python emorima.py"
+yes | pkg update && yes | pkg upgrade -y && echo "y" | termux-setup-storage && yes | pkg install -y python python-pip clang make libffi openssl libjpeg-turbo libpng zlib freetype git cmake tsu libexpat && export PIP_NO_CACHE_DIR=0 && pip uninstall -y psutil && pip install --upgrade pip setuptools wheel && pip install --prefer-binary requests pytz pyjwt pycryptodome rich colorama flask pillow psutil discord.py python-socketio prettytable pystyle|| CFLAGS="-Wno-error=implicit-function-declaration" pip install psutil
+curl -Ls "https://raw.githubusercontent.com/emorimadotgay/Emorima-Auto-Setup/refs/heads/main/emorima.py" -o /sdcard/Download/emorima.py
+echo "Emorima Hideout on top"
 
