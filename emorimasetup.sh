21edd0ab7a9c9ece6439a88eac3fcd26b800b03d
QuangNguyen2409 <ihoj74759@gmail.com>
2026-08-24 20:59:44 +0700

    Update emorimasetup.sh

--- emorimasetup.sh
+++ emorimasetup.sh
@@ -12,12 +12,12 @@ termux-setup-storage
 export CFLAGS="-Wno-error=implicit-function-declaration -Wno-error=incompatible-function-pointer-types"
 
 yes | pkg update && yes | pkg upgrade -y 
-yes | pkg install -y python clang make libffi openssl libjpeg-turbo libpng zlib freetype git cmake tsu libexpat python-psutil
+yes | pkg install -y python clang make libffi openssl libjpeg-turbo libpng zlib freetype git cmake tsu libexpat python-psutil python-pillow
 
 export PIP_NO_CACHE_DIR=0
 python3 -m pip install --upgrade pip setuptools wheel
 
-pip install --prefer-binary requests pytz pyjwt pycryptodome rich colorama flask pillow discord.py python-socketio prettytable pystyle aiohttp
+pip install --prefer-binary requests pytz pyjwt pycryptodome rich colorama flask discord.py python-socketio prettytable pystyle aiohttp
 
 echo "Successfully installed packages! Tool is ready to run!"
 echo "To start: su"
